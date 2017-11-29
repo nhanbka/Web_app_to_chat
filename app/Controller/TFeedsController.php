b5c927be8598807208cf0eedeb61ec29339ea334
Trọng Nhân Lê <nhanletrong999@gmail.com>
2017-11-29 13:34:51 +0700

    Ver 1.0.0

--- app/Controller/TFeedsController.php
+++ app/Controller/TFeedsController.php
@@ -1,10 +1,25 @@
 <?php
 	class TFeedsController extends AppController{
-	public $name="TFeeds";
+	public $name="Tfeeds";
 	
-	function index(){
-		$data = $this->TFeed->find('all');
-		$this->set("index",$data);
+	function feed(){
+		$data = $this->Tfeed->find('all', array ('order'=>array('id'=>'desc')));
+		$this->set("data",$data);
+
+		if(isset($_POST) && $_POST)
+			{	
+				$this->Tfeed->set(array(
+				'name' => $_POST['name'],
+				'message' => $_POST['msg'],
+				'create_at' => date("Y-m-d H:i:s")
+				));
+				$this->Tfeed->save();
+			}
+		$data = $this->Tfeed->find('all', array ('order'=>array('id'=>'desc')));
+		$this->set("data",$data);
 	}
+
 }
-?>
+
+
+
